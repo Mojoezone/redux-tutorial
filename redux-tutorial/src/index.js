216b214c3bdfc83c8bcbabeaadc70c6dccbf9b19
mojoezone@gmail.com <mojoezone@gmail.com>
2018-05-07 18:40:02 -0700

    second commit after action

--- redux-tutorial/src/index.js
+++ redux-tutorial/src/index.js
@@ -4,5 +4,35 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import productReducer from './reducers/product-reducer';
+import userReducer from './reducers/user-reducer';
+
+import {Provider} from 'react-redux';
+import {combineReducers, createStore} from 'redux';
+
+
+// change act to es6 param type, payload
+
+
+const allReducer = combineReducers({
+    products: productReducer,
+    user: userReducer,
+});
+
+const store = createStore(
+    allReducer, 
+    {
+        products: [{name: 'iPhone'}],
+        user: 'Micheal'
+    },
+    window.devToolsExtension && window.devToolsExtension()
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+    
+);
 registerServiceWorker();
